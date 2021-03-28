dff2f95945c3b62d21c3748fa1cd9769694bdb4e
ohlulu <z30262226@gmail.com>
2021-03-28 11:52:19 +0800

    [ Init ] : project

---
+++ Shared/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Shared
+//
+//  Created by Ohlulu on 2021/3/28.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
